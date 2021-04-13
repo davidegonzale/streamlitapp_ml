2833041e7b554a3f9bac7bcfbd298f9b589171ce
David Gonzalez <46650187+davidegonzale@users.noreply.github.com>
2021-04-13 13:51:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-import plotly_express as px
+import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style('darkgrid')
